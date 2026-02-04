e848fb30f034cadc94b1ec29023727e42b7aa4f7
Trenton Scott <InsaneGriot@Outlook.com>
2026-02-03 23:41:08 -0500

    ConditionNode

---
+++ AIBehavior/BehaviorConditionNode.cs
@@ -0,0 +1,8 @@
+﻿namespace Zin.AIBehavior
+{
+    public class BehaviorConditionNode(string name, Func<bool> condition) : BehaviorNode(name)
+    {
+        public Func<bool> Condition { get; set; } = condition;
+        public override Status Process() => Condition() ? Status.Success :Status.Failure;
+    }
+}
---
+++ AIBehavior/BehaviorSelector.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Zin.AIBehavior
+{
+    public class BehaviorSelector() : BehaviorNode("BehaviorSelector")
+    {
+        public override Status Process()
+        {
+            Status childStatus = Children[CurrentChildIndex].Process();
+
+            if (childStatus is Status.Running)
+                return Status.Running;
+
+            if (childStatus is Status.Success)
+            {
+                CurrentChildIndex = 0;
+                return Status.Success;
+            }
+
+            return CurrentChildIndex++ >= Children.Count
+                ? (CurrentChildIndex = 0) == 0
+                    ? Status.Failure
+                        : Status.Failure
+                : Status.Running;
+        }
+    }
+}
