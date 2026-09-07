424129e93a63dc169baa12929f701d9a851f1d6c
Trenton Scott <InsaneGriot@Outlook.com>
2026-09-06 23:06:21 -0400

    UCS and name changes

---
+++ General/Algorithms/UCS.cs
@@ -0,0 +1,83 @@
+﻿using Zin.General.Interfaces;
+
+namespace Zin.General.Algorithms
+{
+    public class UCS : ISearchAlgorithm
+    {
+        public List<GeneralNode> Search(GeneralNode start, GeneralNode goal)
+        {
+            List<GeneralNode> frontier = new();
+            start.TotalCost = 0;
+            frontier.Add(start);
+
+            while (frontier.Count > 0)
+            {
+                GeneralNode current = GetLowestCostNode(frontier);
+
+                frontier.Remove(current);
+                if (current.Visited)
+                    continue;
+
+                current.Visited = true;
+
+                Console.WriteLine(
+                    "UCS: " + current.Name +
+                    " Cost: " + current.TotalCost);
+
+                if (current == goal)
+                {
+                    return BuildPath(start, goal);
+                }
+
+                foreach (GeneralNode child in current.Children)
+                {
+                    int newCost = current.TotalCost + child.Cost;
+
+                    if (newCost < child.TotalCost)
+                    {
+                        child.TotalCost = newCost;
+                        child.Parent = current;
+
+                        frontier.Add(child);
+                    }
+                }
+
+            }
+            return new List<GeneralNode>();
+        }
+
+        private GeneralNode GetLowestCostNode(List<GeneralNode> frontier)
+        {
+            GeneralNode lowest = frontier[0];
+
+            foreach (GeneralNode node in frontier)
+            {
+                if (node.TotalCost < lowest.TotalCost)
+                {
+                    lowest = node;
+                }
+            }
+
+            return lowest;
+        }
+
+        private List<GeneralNode> BuildPath(GeneralNode start, GeneralNode goal)
+        {
+            List<GeneralNode> path = new List<GeneralNode>();
+            GeneralNode? current = goal;
+
+            while (current != null)
+            {
+                path.Add(current);
+
+                if (current == start)
+                    break;
+
+                current = current.Parent;
+            }
+
+            path.Reverse();
+            return path;
+        }
+    }
+}
---
+++ General/Navigator.cs
@@ -0,0 +1,30 @@
+﻿using Zin.General.Interfaces;
+
+namespace Zin.General
+{
+    public class Navigator(ISearchAlgorithm searchAlgorithm)
+    {
+        private ISearchAlgorithm searchAlgorithm = searchAlgorithm;
+
+        public void SetAlgorithm(ISearchAlgorithm searchAlgorithm)
+        {
+            this.searchAlgorithm = searchAlgorithm;
+        }
+
+        public List<GeneralNode> FindPath(GeneralNode start, GeneralNode goal)
+        {
+            ResetGraph(start);
+            return searchAlgorithm.Search(start, goal);
+        }
+
+        private void ResetGraph(GeneralNode generalNode)
+        {
+            generalNode.Reset();
+
+            foreach (GeneralNode child in generalNode.Children)
+            {
+                ResetGraph(child);
+            }
+        }
+    }
+}
