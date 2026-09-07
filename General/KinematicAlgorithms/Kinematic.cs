f14d50f9a4f0fe6cf337b840a819f4e71a956594
Trenton Scott <InsaneGriot@Outlook.com>
2026-09-07 15:04:27 -0400

    Kinematic classes added

---
+++ General/KinematicAlgorithms/Kinematic.cs
@@ -0,0 +1,23 @@
+﻿using Stride.Core.Mathematics;
+
+namespace Zin.General.KinematicAlgorithms
+{
+    public class Kinematic
+    {
+        public Vector3 Position { get; set; }
+        public float Orientation { get; set; }
+        public Vector3 Velocity { get; set; }
+        public float Rotation { get; set; }
+
+        public void Update(SteeringOutput steering, float time)
+        {
+            float halfTimeSquared = 0.5f * time * time;
+
+            Position += Velocity * time + steering.Linear * halfTimeSquared;
+            Orientation += Rotation * time + steering.Angular * halfTimeSquared;
+
+            Velocity += steering.Linear * time;
+            Rotation += steering.Angular * time;
+        }
+    }
+}
---
+++ General/KinematicAlgorithms/SteeringOutput.cs
@@ -0,0 +1,10 @@
+﻿using Stride.Core.Mathematics;
+
+namespace Zin.General.KinematicAlgorithms
+{
+    public class SteeringOutput
+    {
+        public Vector3 Linear { get; set; }
+        public float Angular { get; set; }
+    }
+}
